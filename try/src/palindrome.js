c4a70b18e02e3fb234475b06d88f7cda3ba75c6e
osofariu <ovi@sofariu.com>
2017-02-13 08:07:33 -0500

    added more tests for palindrome

--- try/src/palindrome.js
+++ try/src/palindrome.js
@@ -1,9 +1,19 @@
 class Palindrome {
 
+
     check(str) {
-        let reversed = str.split("").reverse().join("");
-        return str === reversed;
+        let cleanStr = str.split("").filter(isNotWhitespace).join("");
+        if (cleanStr === '') {
+            return false;
+        }
+
+        let reversed = cleanStr.split("").reverse().join("");
+        return cleanStr === reversed;
     }
+}
+
+function isNotWhitespace(ch) {
+    return ch.match(/\S/) != null;
 }
 
 module.exports = Palindrome;
---
+++ try/test/palindrome_test.js
@@ -0,0 +1,63 @@
+var expect = require('chai').expect;
+var Palindrome = require('../src/palindrome.js');
+
+var palindrome;
+
+beforeEach(function() {
+    palindrome = new Palindrome();
+});
+
+describe('basic palindrome without punctiation', function() {
+
+    it('an empty phrase is not a palindrome', function () {
+        expect(isPalindrome('')).not.to.be.true;
+    });
+
+    it('ignores spaces', function () {
+        expect(isPalindrome('  ')).to.be.false;
+        expect(isPalindrome('m o m')).to.be.true;
+    });
+
+    it('recognizes a simple palindrome', function() {
+        expect(isPalindrome('mom')).to.be.true;
+    });
+
+    it('not a palindrome in a simple word', function () {
+        expect(isPalindrome('abc')).to.not.be.true;
+    });
+
+    function isPalindrome(phrase) {
+        return palindrome.check(phrase);
+    }
+});
+
+
+
+/*
+
+This  siht  // ok - case doesn't matter
+bob
+boob
+table el bat // ok - we ignore spaces:
+''
+' '
+'  '
+a
+aa
+aaa
+aaaa
+
+abba
+aba
+
+madam
+madam or nurses run
+
+- empty (ok)
+- one character (ok)
+- two same (ok)
+- two different (bad)
+- string of any length of one charater (ok) -- property test ideal
+- 
+
+*/
